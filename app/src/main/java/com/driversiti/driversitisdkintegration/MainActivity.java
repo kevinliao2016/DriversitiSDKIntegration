e699c8780760867b821494ac301add2ffd133b66
kevin2016 <he.liao@driversiti.com>
2017-01-18 09:25:41 -0500

    Request Permission At Runtime

--- app/src/main/java/com/driversiti/driversitisdkintegration/MainActivity.java
+++ app/src/main/java/com/driversiti/driversitisdkintegration/MainActivity.java
@@ -1,5 +1,9 @@
 package com.driversiti.driversitisdkintegration;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,100 +33,63 @@ import com.driversiti.driversitisdk.driversiti.event.SpeedRestoredEvent;
 import com.driversiti.driversitisdk.driversiti.event.TripEndEvent;
 import com.driversiti.driversitisdk.driversiti.event.TripStartEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.util.UUID.randomUUID;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_ID = 1;
     UserManager mUserManager;
     DriversitiSDK mDriversitiSDK;
     DriversitiEventListener mListener;
     MainActivity mContext;
+    Button mRegistrationButton;
+    private static boolean mIsDriversitiSetup = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DriversitiConfiguration driversitiConfiguration = new DriversitiConfiguration.ConfigurationBuilder()
-                .setContext(this)
-                .setApplicationId("")
-                .setDetectionMode(DriversitiConfiguration.DetectionMode.AUTO_ON)
-                .setEnabledEvents(DriversitiConfiguration.getEnabledEvents())
-                .setEnableVehicleIdentification(true)
-                .setSetupHandler(new DriversitiConfiguration.SetupHandler() {
-                    @Override
-                    public void onSetupSuccess() {
-                        Log.i(LOG_TAG, "Driversiti setup successfull");
-                    }
+        mContext = this;
+        mRegistrationButton = (Button) findViewById(R.id.register_button);
+        // in case of permission request is needed, delay configuration until necessary permissions are granted
+        // additionally, check if configuration is already setup, if yes, skip the following process
+        if (!needRequestPermission() && !mIsDriversitiSetup) {
+            Log.i(LOG_TAG, "OnCreate setupDriversitiConfiguration()");
+            setupDriversitiConfiguration();
+        }
 
-                    @Override
-                    public void onSetupFailure(DriversitiException errorMessage) {
-                        Log.i(LOG_TAG, "Driversiti setup error: " + errorMessage);
-                    }
-                })
-                .build();
-        Button registrationButton = (Button) findViewById(R.id.register_button);
-
-
-        Driversiti.setConfiguration(driversitiConfiguration);
-        mDriversitiSDK = Driversiti.getSDK();
-        mListener = getDriversitiEventListener();
-        mUserManager = mDriversitiSDK.getUserManager();
-        registrationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final User userRegistrationRequest = getApioUserData();
-                OnCompleteTaskHandler callback = new OnCompleteTaskHandler() {
-                    @Override
-                    public void onSuccess(Object result) {
-                        User newUser = null;
-                        if (result != null && result instanceof User) {
-                            newUser = (User) result;
-                        } else {
-                            Log.e(LOG_TAG, "onSuccess() returned a null result, this shouldn't happen...");
-                            this.onFailure(new DriversitiException("Error in create user response, unable to parse a null User object"));
-                            return;
-                        }
-                        Log.d(LOG_TAG, "onSuccess() returned a newUser: " + newUser.toString());
-                        //Set the unique Id
-
-                        if(mUserManager == null){
-                            mUserManager = Driversiti.getSDK().getUserManager();
-                        }
-                        // Log the user in.
-                        mUserManager.loginUser(newUser);
-
-
-                    }
-
-                    @Override
-                    public void onFailure(Exception errorMessage) {
-                        Log.e(LOG_TAG, "Exception during User Registration: " + errorMessage.getMessage(), errorMessage);
-                        Toast.makeText(getApplicationContext(), "Unable to register, please try again: " + errorMessage.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                };
-                mUserManager.addUser(userRegistrationRequest, callback);
-
-            }
-        });
-
+        if (mIsDriversitiSetup) {
+            mDriversitiSDK = Driversiti.getSDK();
+            mListener = getDriversitiEventListener();
+            setupRegistrationButton();
+        }
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mDriversitiSDK.addEventListener(mListener);
-    }
+        if (mIsDriversitiSetup) {
+            Log.i(LOG_TAG, "onResume Adding Listener");
+            mDriversitiSDK.addEventListener(mListener);
+        }
 
+    }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mDriversitiSDK.removeEventListener(mListener);
+        if (mIsDriversitiSetup) {
+            Log.i(LOG_TAG, "Removing Listener");
+            mDriversitiSDK.removeEventListener(mListener);
+        }
     }
 
-    private User getApioUserData(){
+    private User getApioUserData() {
         User user = new UserBuilder()
                 .setFirstName("android")
                 .setLastName("dev")
@@ -134,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         return user;
     }
 
-    private void postToUiThreadHelper(Runnable runnable){
+    private void postToUiThreadHelper(Runnable runnable) {
         mContext.runOnUiThread(runnable);
     }
 
@@ -146,7 +113,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -158,7 +126,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -170,7 +139,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -182,7 +152,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -199,7 +170,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -212,7 +184,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -225,7 +198,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -244,7 +218,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + speedExceededEvent.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + speedExceededEvent.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + speedExceededEvent.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -257,7 +232,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + speedRestoredEvent.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + speedRestoredEvent.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + speedRestoredEvent.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -270,7 +246,8 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
@@ -284,12 +261,143 @@ public class MainActivity extends AppCompatActivity {
                 Runnable runnable = new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG);
+                        Log.i(LOG_TAG, "Posting " + event.getEventType().toString());
+                        Toast.makeText(mContext, "event: " + event.getEventType().toString(), Toast.LENGTH_LONG).show();
                     }
                 };
                 postToUiThreadHelper(runnable);
 
             }
         };
+
+
+    }
+
+    public void setupRegistrationButton() {
+        // must register a user in order to allow sdk to initiate
+        mUserManager = mDriversitiSDK.getUserManager();
+        mRegistrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mUserManager.getActiveUser() != null) {
+                    Toast.makeText(mContext, "User already registered", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                final User userRegistrationRequest = getApioUserData();
+                OnCompleteTaskHandler callback = new OnCompleteTaskHandler() {
+                    @Override
+                    public void onSuccess(Object result) {
+                        User newUser = null;
+                        if (result != null && result instanceof User) {
+                            newUser = (User) result;
+                        } else {
+                            Log.e(LOG_TAG, "onSuccess() returned a null result, this shouldn't happen...");
+                            this.onFailure(new DriversitiException("Error in create user response, unable to parse a null User object"));
+                            return;
+                        }
+                        Log.d(LOG_TAG, "onSuccess() returned a newUser: " + newUser.toString());
+                        //Set the unique Id
+
+                        if (mUserManager == null) {
+                            mUserManager = Driversiti.getSDK().getUserManager();
+                        }
+                        // Log the user in.
+                        mUserManager.loginUser(newUser);
+
+
+                    }
+
+                    @Override
+                    public void onFailure(Exception errorMessage) {
+                        Log.e(LOG_TAG, "Exception during User Registration: " + errorMessage.getMessage(), errorMessage);
+                        Toast.makeText(getApplicationContext(), "Unable to register, please try again: " + errorMessage.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                };
+                mUserManager.addUser(userRegistrationRequest, callback);
+
+            }
+        });
+    }
+
+
+    public void setupDriversitiConfiguration() {
+        DriversitiConfiguration driversitiConfiguration = new DriversitiConfiguration.ConfigurationBuilder()
+                .setContext(this)
+                .setApplicationId("")
+                .setDetectionMode(DriversitiConfiguration.DetectionMode.AUTO_ON)
+                .setEnabledEvents(DriversitiConfiguration.getEnabledEvents())
+                .setEnableVehicleIdentification(true)
+                .setSetupHandler(new DriversitiConfiguration.SetupHandler() {
+                    @Override
+                    public void onSetupSuccess() {
+                        Log.i(LOG_TAG, "Driversiti setup successfull");
+                    }
+
+                    @Override
+                    public void onSetupFailure(DriversitiException errorMessage) {
+                        Log.i(LOG_TAG, "Driversiti setup error: " + errorMessage);
+                    }
+                })
+                .build();
+
+        Driversiti.setConfiguration(driversitiConfiguration);
+        mIsDriversitiSetup = true;
+    }
+
+    public boolean needRequestPermission() {
+        List<String> listPermissionNeeded = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            listPermissionNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (ContextCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            listPermissionNeeded.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if (!listPermissionNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(mContext,
+                    listPermissionNeeded.toArray(new String[listPermissionNeeded.size()]), REQUEST_ID);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ID: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    // configuration is setup here once permissions are granted
+                    if (!mIsDriversitiSetup) {
+                        setupDriversitiConfiguration();
+                        mDriversitiSDK = Driversiti.getSDK();
+                        mListener = getDriversitiEventListener();
+                        Log.i(LOG_TAG, "Permission Request  Adding Listener");
+                        mDriversitiSDK.addEventListener(mListener);
+                        setupRegistrationButton();
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
